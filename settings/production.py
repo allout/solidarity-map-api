d0ec680a105addeedf26648346d53ecb5a26a86b
Timmeh <timmygee@users.noreply.github.com>
2020-06-18 21:23:56 +0200

    Fix mongo env reference

--- settings/production.py
+++ settings/production.py
@@ -1,4 +1,13 @@
 import os
+from environs import Env
 from base import *
 
-MONGO_URI = os.environ.get('MONGO_URI')
+env = Env()
+
+# DB connection
+MONGO_URI = env.str('MONGO_URI')
+
+# CORS whitelist for client domains.
+# This can be an env var which contains a comma separated list of domains allowed to make requests of this server
+X_DOMAINS = env.list('X_DOMAINS', default=[])
+print(X_DOMAINS)
